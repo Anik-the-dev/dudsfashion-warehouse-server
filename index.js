ad5338fd01205fbdce0950ade712bec72776f059
Anik-the-dev <niksctg00@gmail.com>
2022-05-03 19:19:09 +0600

    data get from server

--- index.js
+++ index.js
@@ -12,9 +12,36 @@ const port = process.env.PORT || 5000;
 
 // mongo Database Atlas connection.........
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k6gvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://dusfashionspanik22:nVsYiJb8cybBLCvT@cluster0.k6gvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+async function run() {
+    
+    
+    try {
+        
+        await client.connect();
+        console.log(`Connection establish21`)
+        const database = client.db('dudfashion').collection('inventoryItems-Dress');
+
+        // get data from mongo db
+        app.get('/users', async (req, res)=>{
+            const query = {}
+            const cursor = database.find(query)
+            const usersdb = await cursor.toArray()
+            console.log(usersdb)
+            res.send(usersdb)
+        })
+
+    } finally {
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log("Look mama, Anik Listening 5000")
+    console.log("Look mama, Anik Listening 4000")
 })
